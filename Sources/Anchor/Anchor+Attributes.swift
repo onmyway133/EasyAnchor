296009072de1a5a2a44b4278c2d8fd070830228d
Khoa Pham <onmyway133@gmail.com>
2017-05-06 10:08:07 +0200

    Add Anchor+Attributes

---
+++ Sources/Anchor/Anchor+Attributes.swift
@@ -0,0 +1,103 @@
+import UIKit
+
+public extension Anchor {
+  var left: Anchor {
+    pins.append(Pin(.left))
+    return self
+  }
+
+  var right: Anchor {
+    pins.append(Pin(.right))
+    return self
+  }
+
+  var top: Anchor {
+    pins.append(Pin(.top))
+    return self
+  }
+
+  var bottom: Anchor {
+    pins.append(Pin(.bottom))
+    return self
+  }
+
+  var leading: Anchor {
+    pins.append(Pin(.leading))
+    return self
+  }
+
+  var trailing: Anchor {
+    pins.append(Pin(.trailing))
+    return self
+  }
+
+  var width: Anchor {
+    pins.append(Pin(.width))
+    return self
+  }
+
+  var height: Anchor {
+    pins.append(Pin(.height))
+    return self
+  }
+
+  var centerX: Anchor {
+    pins.append(Pin(.centerX))
+    return self
+  }
+
+  var centerY: Anchor {
+    pins.append(Pin(.centerY))
+    return self
+  }
+
+  var lastBaseline: Anchor {
+    pins.append(Pin(.lastBaseline))
+    return self
+  }
+
+  var firstBaseline: Anchor {
+    pins.append(Pin(.firstBaseline))
+    return self
+  }
+
+  var leftMargin: Anchor {
+    pins.append(Pin(.leftMargin))
+    return self
+  }
+
+  var rightMargin: Anchor {
+    pins.append(Pin(.rightMargin))
+    return self
+  }
+
+  var topMargin: Anchor {
+    pins.append(Pin(.topMargin))
+    return self
+  }
+
+  var bottomMargin: Anchor {
+    pins.append(Pin(.bottomMargin))
+    return self
+  }
+
+  var leadingMargin: Anchor {
+    pins.append(Pin(.leadingMargin))
+    return self
+  }
+
+  var trailingMargin: Anchor {
+    pins.append(Pin(.trailingMargin))
+    return self
+  }
+
+  var centerXWithinMargins: Anchor {
+    pins.append(Pin(.centerXWithinMargins))
+    return self
+  }
+
+  var centerYWithinMargins: Anchor {
+    pins.append(Pin(.centerYWithinMargins))
+    return self
+  }
+}
