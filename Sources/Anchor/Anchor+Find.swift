72ef8c081822310028a6ad25a3c5670baaf6ce0c
Khoa Pham <onmyway133@gmail.com>
2017-05-06 10:35:44 +0200

    Find constraint for attribute

--- Sources/Anchor/Anchor+Find.swift
+++ Sources/Anchor/Anchor+Find.swift
@@ -16,4 +16,23 @@ public extension Anchor {
 
 public extension Anchor.Find {
 
+  func constraint(_ attribute: NSLayoutAttribute) -> NSLayoutConstraint? {
+    var constraints = view.superview?.constraints
+
+    if attribute == .width || attribute == .height {
+      constraints = view.constraints
+    }
+
+    return constraints?.filter({
+      guard $0.firstAttribute == attribute else {
+        return false
+      }
+
+      guard $0.firstItem as? NSObject == view || $0.secondItem as? NSObject == view else {
+        return false
+      }
+
+      return true
+    }).first
+  }
 }
