29d1ec5e0759a4ad1fd134623c77e227c1b44b05
Khoa Pham <onmyway133@gmail.com>
2017-05-06 09:11:31 +0200

    Add testPins

--- AnchorsTests/Tests.swift
+++ AnchorsTests/Tests.swift
@@ -1,35 +1,12 @@
-//
-//  Tests.swift
-//  Anchors
-//
-//  Created by Khoa Pham on 05/05/2017.
-//  Copyright © 2017 Khoa Pham. All rights reserved.
-//
-
 import XCTest
+@testable import Anchors
 
 class Tests: XCTestCase {
 
-  override func setUp() {
-    super.setUp()
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+  func testPins() {
+    let anchor = Anchor(view: UIView()).center
+    XCTAssertEqual(anchor.pins.count, 2)
+    XCTAssertEqual(anchor.pins.contains(where: { $0.attribute == .centerX }), true)
+    XCTAssertEqual(anchor.pins.contains(where: { $0.attribute == .centerY }), true)
   }
-
-  override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    super.tearDown()
-  }
-
-  func testExample() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-  }
-
-  func testPerformanceExample() {
-    // This is an example of a performance test case.
-    self.measure {
-      // Put the code you want to measure the time of here.
-    }
-  }
-
 }
