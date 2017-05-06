63c6134444bf78625a799958c2b8bc222fbf2811
Khoa Pham <onmyway133@gmail.com>
2017-05-06 09:18:42 +0200

    Add testConfig

--- AnchorsTests/Tests.swift
+++ AnchorsTests/Tests.swift
@@ -18,4 +18,13 @@ class Tests: XCTestCase {
     XCTAssertEqual(anchor.pins.contains(where: { $0.attribute == .bottom && $0.constant == 3 }), true)
     XCTAssertEqual(anchor.pins.contains(where: { $0.attribute == .right && $0.constant == 4 }), true)
   }
+
+  func testConfig() {
+    let anchor = Anchor(view: UIView()).center.constant(10).multiplier(1.5).priority(999)
+    XCTAssertEqual(anchor.pins.count, 2)
+    XCTAssertEqual(anchor.multiplierValue, 1.5)
+    XCTAssertEqual(anchor.priorityValue, 999)
+    XCTAssertEqual(anchor.pins.contains(where: { $0.attribute == .centerX && $0.constant == 10 }), true)
+    XCTAssertEqual(anchor.pins.contains(where: { $0.attribute == .centerY && $0.constant == 10 }), true)
+  }
 }
