fca85f588c60f338e027e955cb09d4b301c2e6b7
Khoa Pham <onmyway133@gmail.com>
2017-05-06 09:44:36 +0200

    Rename to activate

--- Sources/Anchors.swift
+++ Sources/Anchors.swift
@@ -2,7 +2,7 @@ import UIKit
 
 // MARK: - Public
 
-public func anchor(_ anchors: Anchor ...) {
+public func activate(_ anchors: Anchor ...) {
   anchors.forEach({
     $0.output().forEach({
       $0.isActive = true
