5d84dbc079616c8774fbc4f7bce0c4bcb061f6c9
Khoa Pham <onmyway133@gmail.com>
2017-05-09 00:20:56 +0200

    Add Color

---
+++ Example/AnchorsDemo/AnchorsDemo/Sources/Color.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+struct Color {
+  static let background = UIColor.lightGray
+  static let color1 = UIColor.lightGray
+  static let color2 = UIColor.lightGray
+  static let color3 = UIColor.lightGray
+  static let color4 = UIColor.lightGray
+  static let color5 = UIColor.lightGray
+}
