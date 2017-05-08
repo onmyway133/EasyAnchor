2dd059feaf78bd791281d31f849c771f5cc3e0ed
Khoa Pham <onmyway133@gmail.com>
2017-05-08 22:58:39 +0200

    Use static funcs

--- Sources/Builder/Builder.swift
+++ Sources/Builder/Builder.swift
@@ -4,17 +4,17 @@ import UIKit
 public class Builder {
 
   /// Build a paging scrollView
-  public func paging(_ scrollView: UIScrollView, views: [UIView]) -> Builder.Paging {
+  public static func paging(_ scrollView: UIScrollView, views: [UIView]) -> Builder.Paging {
     return Paging(scrollView: scrollView, views: views)
   }
 
   /// Apply the same anchor to other views
-  public func align(_ anchor: Anchor, to views: [UIView]) -> Builder.Apply {
+  public static func align(_ anchor: Anchor, to views: [UIView]) -> Builder.Apply {
     return Apply(anchor: anchor, views: views)
   }
 
   /// Distribute views horizontally
-  public func distribute(horizontally views: [UIView], spacing: CGFloat) -> Builder.Distribute {
+  public static func distribute(horizontally views: [UIView], spacing: CGFloat) -> Builder.Distribute {
     return Distribute(views: views, spacing: spacing)
   }
 }
