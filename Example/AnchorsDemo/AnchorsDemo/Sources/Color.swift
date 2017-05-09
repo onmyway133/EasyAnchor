c6262953e4bdf459d0ae0800b7d3b26af3466d92
Khoa Pham <onmyway133@gmail.com>
2017-05-09 21:05:21 +0200

    Add some flat colors

--- Example/AnchorsDemo/AnchorsDemo/Sources/Color.swift
+++ Example/AnchorsDemo/AnchorsDemo/Sources/Color.swift
@@ -2,10 +2,9 @@ import UIKit
 import Hue
 
 struct Color {
-  static let background = UIColor.lightGray
-  static let color1 = UIColor.lightGray
-  static let color2 = UIColor.lightGray
-  static let color3 = UIColor.lightGray
-  static let color4 = UIColor.lightGray
-  static let color5 = UIColor.lightGray
+  static let background = UIColor(hex: "#ecf0f1")
+  static let color1 = UIColor(hex: "#e74c3c")
+  static let color2 = UIColor(hex: "#3498db")
+  static let color3 = UIColor(hex: "#16a085")
+  static let color4 = UIColor(hex: "#9b59b6")
 }
