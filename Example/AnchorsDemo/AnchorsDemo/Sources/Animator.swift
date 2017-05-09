f87174b5651c62edaebffa2f1aeb419900d673db
Khoa Pham <onmyway133@gmail.com>
2017-05-09 21:13:43 +0200

    Update Animator

--- Example/AnchorsDemo/AnchorsDemo/Sources/Animator.swift
+++ Example/AnchorsDemo/AnchorsDemo/Sources/Animator.swift
@@ -1,13 +1,30 @@
 import UIKit
 
 class Animator {
-  func animate(animation: @escaping () -> Void, completion: @escaping () -> Void) {
-    let animator = UIViewPropertyAnimator(duration: 0.25, dampingRatio: 1)
-    animator.addAnimations(animation)
+  class Animation {
+    let block: () -> Void
+
+    init(block: @escaping () -> Void) {
+      self.block = block
+    }
+  }
+
+  let animations: [Animation]
+  var index = 0
+
+  init(animations: [Animation]) {
+    self.animations = animations
+  }
+
+  func animate() {
+    let animation = animations[index]
+
+    let animator = UIViewPropertyAnimator(duration: 1, dampingRatio: 1)
+    animator.addAnimations(animation.block)
     animator.addCompletion({ _ in
-      completion()
+      self.animate()
     })
 
-    animator.startAnimation()
+    animator.startAnimation(afterDelay: 2)
   }
 }
