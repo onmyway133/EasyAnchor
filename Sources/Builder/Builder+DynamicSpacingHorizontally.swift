c0a0279b6a3faf65770a50f6ee37b34f8bce986f
Khoa Pham <onmyway133@gmail.com>
2017-05-13 14:27:23 +0200

    Add dynamicSpacingHorizontally

---
+++ Sources/Builder/Builder+DynamicSpacingHorizontally.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+public extension Builder {
+  public class DynamicSpacingHorizontally: ConstraintProducer {
+
+    let views: [UIView]
+    let spacing: CGFloat
+
+    init(views: [UIView], spacing: CGFloat) {
+      self.views = views
+      self.spacing = spacing
+    }
+
+    public func constraints() -> [NSLayoutConstraint] {
+      var anchors: [Anchor] = []
+      views.enumerated().forEach({ i, view in
+        if i < views.count - 1 {
+          anchors.append(view.anchor.trailing.equal.to(views[i+1].anchor.leading).constant(-spacing))
+        }
+      })
+
+      return Group(producers: anchors).constraints
+    }
+  }
+}
